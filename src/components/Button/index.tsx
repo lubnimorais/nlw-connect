daffa879c6abdcc657a02335351b0ad497763b76
Lubni Morais <lubni.morais@gmail.com>
2025-02-24 16:17:04 -0300

    settings: configure Biome

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,33 +1,28 @@
-import {
-  type ComponentProps,
-  HTMLAttributes,
-  HTMLProps,
-  ReactNode,
-} from 'react'
+import type { ComponentProps } from 'react';
 
 interface IButtonProps extends ComponentProps<'button'> {}
 
 export function Button({ ...rest }: IButtonProps) {
   return (
     <button
-      className="
-      w-full 
-      h-12
-      flex
-      items-center
-      justify-between
-      px-5 
-      bg-gray-500 
-      text-blue 
-      font-semibold 
-      rounded-xl 
-      cursor-pointer
-      transition-colors
-      duration-300
-      hover:bg-blue
-      hover:text-gray-900
-    "
+      className='
+        w-full 
+        h-12
+        flex
+        items-center
+        justify-between
+        px-5 
+        bg-gray-500 
+        text-blue 
+        font-semibold 
+        rounded-xl 
+        cursor-pointer
+        transition-colors
+        duration-300
+        hover:bg-blue
+        hover:text-gray-900
+      '
       {...rest}
     />
-  )
+  );
 }
